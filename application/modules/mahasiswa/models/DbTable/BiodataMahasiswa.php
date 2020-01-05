26f645cf9daacbc12b2ecb5b80b8903510e239c9
root <root@localhost.localdomain>
2020-01-04 19:40:31 -0800

    update salsa

---
+++ application/modules/mahasiswa/models/DbTable/BiodataMahasiswa.php
@@ -0,0 +1,50 @@
+<?php 
+class Mahasiswa_Model_DbTable_BiodataMahasiswa extends Zend_Db_Table {
+
+protected $_name='mahasiswa';
+protected $_primary='Idmhs';
+
+   public function getData(){
+     $db= Zend_Db_Table::getDefaultAdapter();
+     $select=$db->select()
+         ->from($this->_name);
+     $row=$db->fetchAll($select);
+     return $row;
+   }
+   
+public function saveData($data) {
+ $db = Zend_Db_Table::getDefaultAdapter();
+ $id=$db->insert($this->_name,$data);
+ return $id;
+}
+
+public function deleteData($id){
+ $db = Zend_Db_Table::getDefaultAdapter();
+ $id=$db->delete($id);
+}
+   
+public function searchData($filter){
+  $db= Zend_Db_Table::getDefaultAdapter();
+     $select=$db->select()
+         ->from($this->_name);
+     if (isset($filter['nim']) && $filter['nim']!=null) 
+     $select->where('nim=?',$filter['nim']);
+     if (isset($filter['name']) && $filter['name']!=null) 
+     $select->where('name like "%',$filter['name'].'%"');
+      $row=$db->fetchRow($select);
+     return $row;
+     }
+     
+ 
+public function approveData($data,$id){
+ 
+}
+
+public function updateData($data,$key) {
+ $db = Zend_Db_Table::getDefaultAdapter();
+ $id=$db->update($data);
+ return $id;
+}
+
+}
+?>
